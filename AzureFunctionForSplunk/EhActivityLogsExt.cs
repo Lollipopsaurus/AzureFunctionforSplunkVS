b94bb4a8dc5b93882f42d4efdcd979978ba4e012
Dennis K <Den.kilgore@gmail.com>
2018-08-09 13:22:20 -0400

    Update EhActivityLogsExt.cs

--- AzureFunctionForSplunk/EhActivityLogsExt.cs
+++ AzureFunctionForSplunk/EhActivityLogsExt.cs
@@ -35,7 +35,7 @@ namespace AzureFunctionForSplunk
     {
         [FunctionName("EhActivityLogsExt")]
         public static async Task Run(
-            [EventHubTrigger("%input-hub-name-activity-logs%", Connection = "hubConnection")]string[] messages,
+            [EventHubTrigger("insights-operational-logs", Connection = "hubConnection")]string[] messages,
             IBinder blobFaultBinder,
             Binder queueFaultBinder, 
             TraceWriter log)
